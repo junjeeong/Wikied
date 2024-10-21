da43ccfb832793d08c8ec1fd5194888a3ce67266
junjeeong <wn8624@naver.com>
2024-10-21 11:45:46 +0900

    Fix: UI 수정, 로그인하면 icon으로 바뀌고 navItem도 바뀌어야 됨

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import logo from "@/assets/logo/wikied.svg";
 import styles from "./Header.module.css";
 import { useRouter } from "next/router";
+import { SearchInput } from "@/components/ui/SearchInput";
 
 export const Header = () => {
   const router = useRouter();
@@ -15,24 +16,27 @@ export const Header = () => {
 
       <ul className={styles.navList}>
         <li
-          className={`${styles.navItem} ${router.pathname === "/wikilist" ? styles.isActive : ""}`}
-        >
-          <Link href="/wikilist" aria-label="위키 목록">
-            위키 목록
-          </Link>
-        </li>
-        <li
           className={`${styles.navItem} ${router.pathname === "/boards" ? styles.isActive : ""}`}
         >
           <Link href="/boards" aria-label="자유 게시판">
             자유 게시판
           </Link>
         </li>
+        <li
+          className={`${styles.navItem} ${router.pathname === "/wikilist" ? styles.isActive : ""}`}
+        >
+          <Link href="/wikilist" aria-label="모든 위키">
+            모든 위키
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <SearchInput />
+        </li>
       </ul>
 
       <Link href="/signin">
-        <button className={styles.login} aria-label="로그인">
-          로그인
+        <button className={styles.login} aria-label="내 위키 만들기">
+          내 위키 만들기
         </button>
       </Link>
     </div>
