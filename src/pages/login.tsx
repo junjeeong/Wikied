460a05fb4cd3d1436312255fc6868b95746aff02
jjeankim <miyukikj35@gmail.com>
2024-10-23 12:07:25 +0900

    Chore: 로그인 및 회원가입 페이지에서 header 삭제

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,4 @@
 import LoginForm from "@/components/LoginForm"
-import { Header } from "@/components/Layout/Header";
 
 const Login = () => {
   return (
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/Layout/Header";
 import RegisterForm  from "@/components/RegisterForm";
 
 const Login = () => {
