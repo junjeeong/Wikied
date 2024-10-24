94fb8fcba73094f499ca76de9fb2ec13a904c7cf
jjeankim <miyukikj35@gmail.com>
2024-10-24 20:50:15 +0900

    Chore: loginForm test용 코드 삭제

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,5 @@
-import ModalOverlay from "@/components/ui/Modal/ModalOverlay";
-import QuizModal from "@/components/ui/Modal/QuizModal";
 import LoginForm from "@/containers/LoginForm";
-import { useState } from "react";
+
 
 const Login = () => {
 
