10437db744a874fb9b95e30ab430a87a8224c6fc
junjeeong <wn8624@naver.com>
2024-10-24 16:39:53 +0900

    /pages index.tsx 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
 import Image from "next/image";
 import LandingButton from "@/components/ui/Button/LandingButton";
-import titleImg from "@/assets/images/img_home1.png";
-import writeImg from "@/assets/images/img_home4.png";
-import shareImg1 from "@/assets/images/img_item1.png";
-import shareImg2 from "@/assets/images/img_item2.png";
-import shareImg3 from "@/assets/images/img_item3.png";
-import shareImg4 from "@/assets/images/img_item4.png";
-import viewImg1 from "@/assets/images/img_home3.png";
-import viewImg2 from "@/assets/images/img_home2.png";
-import bellImg from "@/assets/images/img_bell.png";
 import useAuthStore from "@/store/AuthStore";
 import Link from "next/link";
 
@@ -17,10 +8,10 @@ const MainPage = () => {
   const linkURL = isLoggedIn ? `/wiki/${user?.id}` : "/login";
 
   const shareItems = [
-    { src: shareImg1, bgColor: "#B2A5FD" },
-    { src: shareImg2, bgColor: "#ADEDDE" },
-    { src: shareImg3, bgColor: "#DEE5F5" },
-    { src: shareImg4, bgColor: "#DEE5F5" },
+    { src: "/images/img_item1.png", bgColor: "#B2A5FD" },
+    { src: "/images/img_item2.png", bgColor: "#ADEDDE" },
+    { src: "/images/img_item3.png", bgColor: "#DEE5F5" },
+    { src: "/images/img_item4.png", bgColor: "#DEE5F5" },
   ];
 
   return (
@@ -40,7 +31,7 @@ const MainPage = () => {
           </Link>
           <Image
             className="absolute z-20 Mobile:top-[368px] top-[461px] w-[498px] h-[590px] Mobile:w-[336px] Mobile:h-[398px]"
-            src={titleImg}
+            src={"/aegims/img_home1"}
             alt="타이틀 섹션 이미지"
           />
         </div>
@@ -74,7 +65,7 @@ const MainPage = () => {
           </div>
           <div className="relative flex-grow flex-shrink-0 aspect-[0.761] w-[192px] Tablet:w-[365px] PC:w-[520px]">
             <Image
-              src={writeImg}
+              src={"/aegims/img_home4.png"}
               className="w-full h-full"
               alt="write 섹션 이미지"
             />
@@ -127,17 +118,17 @@ const MainPage = () => {
 
           <div className="flex flex-col mt-10 Tablet:mt-20 PC:mt-[120px] gap-[10px] Tablet:gap-[22px] PC:gap-[40px]">
             <Image
-              src={viewImg1}
+              src={"/images/img_home3.png"}
               className="w-full h-auto"
               alt="view 이미지 1"
             />
 
             <div className="flex justify-between gap-[10px] Tablet:gap-[22px] PC:gap-[40px] ">
               <div className="bg-purple-100 rounded-[10px]">
-                <Image src={bellImg} alt="bell 이미지" />
+                <Image src={"/images/img_bell.png"} alt="bell 이미지" />
               </div>
               <div>
-                <Image src={viewImg2} alt="view 이미지 2" />
+                <Image src={"/images/img_home2.png"} alt="view 이미지 2" />
               </div>
             </div>
           </div>
