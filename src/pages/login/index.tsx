b302673df5d2482346270d5d2872206ebc833447
jjeankim <miyukikj35@gmail.com>
2024-10-25 11:52:42 +0900

    Feat: 5분 모달 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,41 +1,12 @@
-// import LoginForm from "@/containers/LoginForm";
-
-// const Login = () => {
-//   return (
-//     <>
-//       <LoginForm />
-//     </>
-//   );
-// };
-
-// export default Login;
-import ConnectLosctModal from "@/components/ui/Modal/ConnectLostModal";
-import ModalOverlay from "@/components/ui/Modal/ModalOverlay";
-import { useState } from "react";
 import LoginForm from "@/containers/LoginForm";
 
 const Login = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleModalOpen = () => {
-    setIsOpen(true);
-  };
-
-  const onClose = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
       <LoginForm />
-      <button onClick={handleModalOpen} className="absolute left-5 top-5">
-        열기
-      </button>
-      <ModalOverlay isOpen={isOpen} onClose={onClose}>
-        <ConnectLosctModal/>
-      </ModalOverlay>
     </>
   );
 };
 
 export default Login;
+
