53d95d7c06afa127a520aad93f46d1c7bfc8142a
jjeankim <miyukikj35@gmail.com>
2024-10-25 15:14:48 +0900

    Fix: svg 컴포넌트로 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,43 @@
+// import LoginForm from "@/containers/LoginForm";
+
+// const Login = () => {
+//   return (
+//     <>
+//       <LoginForm />
+//     </>
+//   );
+// };
+
+// export default Login;
+
+import ConnectLostModal from "@/components/ui/Modal/ConnectLostModal";
+import ModalOverlay from "@/components/ui/Modal/ModalOverlay";
 import LoginForm from "@/containers/LoginForm";
+// import QuizModalContainer from "@/containers/QuizModalContainer";
+import { useState } from "react";
 
 const Login = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <LoginForm />
+      <button onClick={handleModalOpen} className="absolute left-5 top-5">
+        열기
+      </button>
+      <ModalOverlay isOpen={isOpen} onClose={onClose}>
+        <ConnectLostModal/>
+      </ModalOverlay>
     </>
   );
 };
 
 export default Login;
-
