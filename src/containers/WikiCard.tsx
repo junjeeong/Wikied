eb85e188cee8a0a29e752668b638269172659411
junjeeong <wn8624@naver.com>
2024-10-25 20:38:51 +0900

    Stlye: WikiCard css 조금 수정

--- src/containers/WikiCard.tsx
+++ src/containers/WikiCard.tsx
@@ -19,15 +19,16 @@ interface WikiCardProps {
 const WikiCard = ({ info }: WikiCardProps) => {
   return (
     <div className="flex flex-col w-full h-full border border-solid border-gray-100 shadow-[0_4px_20px_#00000014] rounded-[16px] cursor-pointer overflow-hidden hover:scale-105 transition-all">
-      <section className="flex flex-grow-1 justify-between align-middle p-[20px] bg-white">
+      <section className="flex flex-grow-1 justify-between p-[20px] bg-white">
         <div>
-          <h2 className="text-2lg font-medium ">{info.name}</h2>
-          <div className="mt-[4px] text-md leading-4 text-gray-300">
-            {info.city} <br /> {info.job}
+          <h2 className="text-2lg font-medium">{info.name}</h2>
+          <div className="flex flex-col gap-[4px] mt-[8px] text-gray-300 text-md leading-4">
+            <p>{info.city}</p>
+            <p>{info.job}</p>
           </div>
         </div>
         <div>
-          <div className="rounded-full w-[64px] h-[64px] text-gray-300 overflow-hidden relative">
+          <div className="rounded-full w-[70px] h-[70px] text-gray-300 overflow-hidden relative">
             {info.image ? (
               <Image
                 src={info.image}
