6a3c406b71e3ac7127cc5c51a008bb03096ce414
jjeankim <miyukikj35@gmail.com>
2024-10-25 12:40:50 +0900

    Feat: 이미지등록 모달 제작중

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,39 @@
+// import LoginForm from "@/containers/LoginForm";
+
+// const Login = () => {
+//   return (
+//     <>
+//       <LoginForm />
+//     </>
+//   );
+// };
+
+// export default Login;
+import ImageAddModal from "@/components/ui/Modal/ImageAddModal";
+import ModalOverlay from "@/components/ui/Modal/ModalOverlay";
 import LoginForm from "@/containers/LoginForm";
+import { useState } from "react";
 
 const Login = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <LoginForm />
+      <button onClick={handleModalOpen} className="absolute left-5 top-5">
+        열기
+      </button>
+      <ModalOverlay isOpen={isOpen} onClose={onClose}>
+        <ImageAddModal />
+      </ModalOverlay>
     </>
   );
 };
