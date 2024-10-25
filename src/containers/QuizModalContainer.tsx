fb4ee2f6ac55c833a5940522fd4f51ec797da432
jjeankim <miyukikj35@gmail.com>
2024-10-25 11:46:33 +0900

    Fix: useAuthStore에서 code 가져와 적용

--- src/containers/QuizModalContainer.tsx
+++ src/containers/QuizModalContainer.tsx
@@ -3,6 +3,7 @@ import { getUserProfile, postProfilePing } from "@/api/profile";
 import { AxiosError } from "axios";
 import QuizModal from "@/components/ui/Modal/QuizModal";
 import ModalOverlay from "@/components/ui/Modal/ModalOverlay";
+import useAuthStore from "@/store/AuthStore";
 
 interface QuizModalContainerProps {
   isOpen: boolean;
@@ -13,15 +14,18 @@ const QuizModalContainer = ({ isOpen, onClose }: QuizModalContainerProps) => {
   const [question, setQuestion] = useState(""); //사용자 질문
   const [quizAnswer, setQuizAnswer] = useState(""); //입력값을 관리
   const [errorMessage, setErrorMessage] = useState(""); //에러값 상태확인
+  const { user } = useAuthStore();
 
-  const code = "e79d333c-d545-48fb-a125-2326aceed778"; //저장되어있을 코드
+  const code = user?.profile.code;
 
   useEffect(() => {
-    const getQuestion = async () => {
-      const res = await getUserProfile(code);
-      setQuestion(res?.data.securityQuestion);
-    };
-    getQuestion();
+    if (code) {
+      const getQuestion = async () => {
+        const res = await getUserProfile(code);
+        setQuestion(res?.data.securityQuestion);
+      };
+      getQuestion();
+    }
   }, [code]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,12 +35,14 @@ const QuizModalContainer = ({ isOpen, onClose }: QuizModalContainerProps) => {
 
   const handleSubmit = async () => {
     try {
-      await postProfilePing(
-        {
-          securityAnswer: quizAnswer,
-        },
-        code
-      );
+      if (code) {
+        await postProfilePing(
+          {
+            securityAnswer: quizAnswer,
+          },
+          code
+        );
+      }
       console.log("수정가능"); //수정 페이지로 이동
     } catch (error) {
       if (error instanceof AxiosError) {
