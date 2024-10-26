91d445d5ff648a2d0d371b5857cffe89fa6b328c
junjeeong <wn8624@naver.com>
2024-10-26 16:15:46 +0900

    Feat: Search페이지 기본 UI 구성 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,24 +1,62 @@
+import { GetServerSidePropsContext } from "next";
 import { getProfiles } from "@/api/profile";
 import { SearchInput } from "@/components/SearchInput";
-import { GetServerSidePropsContext } from "next";
-import { useRouter } from "next/router";
+import PaginationBar from "@/components/PaginationBar";
 
-const getServerSideProps = async (context: GetServerSidePropsContext) => {
+// 프로필 항목의 타입 정의
+interface Profile {
+  id: number; // id를 number로 수정
+  code: string;
+  name: string;
+  job: string;
+  city: string;
+  image: string;
+  nationality: string;
+  updatedAt: string;
+}
+
+// getProfiles의 반환 값 타입 정의
+interface GetProfilesResponse {
+  totalCount: number;
+  list: Profile[];
+}
+
+// 서버 사이드 프로퍼티 가져오기
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const { q } = context.query;
-  const res = getProfiles({ name: q as string });
+  const res: GetProfilesResponse = await getProfiles({ name: q as string });
 
   return {
     props: {
-      list: res,
+      totalCount: res.totalCount,
+      list: res.list,
     },
   };
 };
 
-const SearchPage = ({ list }) => {
-  const router = useRouter();
-  const { q } = router.query;
+// SearchPage 컴포넌트의 프로퍼티 타입 정의
+interface SearchPageProps extends GetProfilesResponse {
+  q: string; // 쿼리 문자열도 props에 추가
+}
 
-  return <SearchInput size="large" value={q as string} />;
+const SearchPage: React.FC<SearchPageProps> = ({ list, totalCount, q }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <SearchInput size="large" value={q} />
+      <div className="mt-[16px] text-lg text-gray-50">
+        "{q}"님을 총<span className="text-green-100">{totalCount}</span>명
+        찾았습니다.
+      </div>
+      <div className="flex felx-col gap-[24px]">
+        {list.map((el) => (
+          <SearchedWikiCard />
+        ))}
+      </div>
+      <PaginationBar />
+    </div>
+  );
 };
 
 export default SearchPage;
