121844c91c8c18273fb20cbf7d9d8c11255fa610
jjeankim <miyukikj35@gmail.com>
2024-10-29 20:36:24 +0900

    Fix: FormInput component로 수정

--- src/containers/LoginFormContainer.tsx
+++ src/containers/LoginFormContainer.tsx
@@ -1,10 +1,10 @@
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { AxiosError } from "axios";
 import Link from "next/link";
 import useAuthStore from "@/store/AuthStore";
 import FilledButton from "@/components/ui/Button/FilledButton";
+import FormInput from "@/components/FormInput";
 
 export interface InputValues {
   email: string;
@@ -34,23 +34,23 @@ const LoginFormContainer = () => {
     } else {
       const user = useAuthStore.getState().user;
       if (user?.profile) {
-        router.push(`/wiki/${user.profile.id}`); //id로 이동 하는 거 맞나요?
-      } else {
-        router.push('/quiz-settings')
-      }
-    }
-  };
-
-  useEffect(()=> {
-    if (isLoggedIn) {
-      const user = useAuthStore.getState().user;
-      if (user?.profile) {
-        router.push(`/wiki/${user.profile.id}`)
+        router.push(`/wiki/${user.profile.name}`);
       } else {
         router.push("/quiz-settings");
       }
     }
-  })
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const user = useAuthStore.getState().user;
+      if (user?.profile) {
+        router.push(`/wiki/${user.profile.id}`);
+      } else {
+        router.push("/quiz-settings");
+      }
+    }
+  });
 
   const handleChange = () => {
     setSubmitError("");
@@ -61,70 +61,40 @@ const LoginFormContainer = () => {
       <div className="w-full max-w-md">
         <h2 className="text-2xl font-semibold mb-[50px] text-center">로그인</h2>
         <form
-          noValidate
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-6 mb-10"
         >
-          <div className="flex flex-col gap-2.5">
-            <label htmlFor="email" className="text-md text-gray-500">
-              이메일
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px]gray100 bg-gray-100 placeholder:text-md placeholder:text-gray-400 ${
-                errors.email ? "outline-red-200" : "outline-green-200"
-              }`}
-              id="email"
-              type="email"
-              placeholder="이메일을 입력해주세요"
-              {...register("email", {
-                required: true,
-                pattern: {
-                  value: /^\S+@\S+\.\S+$/,
-                  message: "이메일 형식으로 작성해 주세요.",
-                },
-              })}
-              onChange={handleChange}
-            ></input>
-            {errors.email && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.email.message}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <label
-              htmlFor="password"
-              className="text-md font-normal text-gray-500"
-            >
-              비밀번호
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px] bg-gray-100 placeholder:text-md placeholder:text-gray4-00 ${
-                errors.password ? "outline-red-200" : "outline-green-200"
-              }`}
-              id="password"
-              type="password"
-              placeholder="비밀번호를 입력해주세요"
-              {...register("password", {
-                required: true,
-                minLength: {
-                  value: 8,
-                  message: "8자 이상 작성해 주세요",
-                },
-              })}
-              onChange={handleChange}
-            ></input>
-            {errors.password && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.password.message}
-              </span>
-            )}
-            {submitError && (
-              <span className="text-xs text-red-200" role="alert">
-                {submitError}
-              </span>
-            )}
-          </div>
+          <FormInput
+            id="email"
+            label="이메일"
+            placeholder="이메일을 입력해주세요"
+            type="text"
+            register={register("email", {
+              required: true,
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "이메일 형식으로 작성해 주세요.",
+              },
+            })}
+            onChange={handleChange}
+            error={errors.email}
+          />
+          <FormInput
+            id="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
+            type="password"
+            register={register("password", {
+              required: true,
+              minLength: {
+                value: 8,
+                message: "8자 이상 작성해 주세요",
+              },
+            })}
+            onChange={handleChange}
+            error={errors.email}
+            submitError={submitError}
+          />
           <FilledButton type="submit" disabled={isSubmitting}>
             로그인
           </FilledButton>
--- src/containers/SignUpFormContainer.tsx
+++ src/containers/SignUpFormContainer.tsx
@@ -5,6 +5,7 @@ import { InputValues } from "@/containers/LoginFormContainer";
 import { AxiosError } from "axios";
 import Link from "next/link";
 import FilledButton from "@/components/ui/Button/FilledButton";
+import FormInput from "@/components/FormInput";
 
 const SignUpFormContainer = () => {
   const {
@@ -21,19 +22,19 @@ const SignUpFormContainer = () => {
 
   const onSubmit = async (data: InputValues) => {
     try {
-      await postSignUp({
+      const res = await postSignUp({
         email: data.email,
         name: data.name,
         password: data.password,
         passwordConfirmation: data.passwordConfirmation,
       });
-      window.alert("가입이 완료되었습니다");
+      alert("가입이 완료되었습니다");
       router.push("/login");
     } catch (err) {
       if (err instanceof AxiosError) {
         if (err.response?.status === 400) {
-          window.alert(err.response.data.message);
-        } else window.alert("이미 사용 중인 이름입니다.");
+          alert(err.response.data.message);
+        } else alert("이미 사용 중인 이름입니다.");
       }
     }
   };
@@ -45,116 +46,63 @@ const SignUpFormContainer = () => {
           회원가입
         </h2>
         <form
-          noValidate
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-6 mb-10"
         >
-          <div className="flex flex-col gap-2.5">
-            <label htmlFor="name" className="text-md text-gray-500">
-              이름
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px]gray100 bg-gray-100 placeholder:text-md placeholder:text-gray-400 ${
-                errors.name ? "outline-red-200" : "outline-green-200"
-              }`}
-              id="name"
-              type="text"
-              placeholder="이름을 입력해 주세요"
-              {...register("name", {
-                required: true,
-                maxLength: {
-                  value: 10,
-                  message: "열 자 이하로 작성해 주세요",
-                },
-              })}
-            ></input>
-            {errors.name && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.name.message}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <label htmlFor="email" className="text-md text-gray-500">
-              이메일
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px]gray100 bg-gray-100 placeholder:text-md placeholder:text-gray-400 ${
-                errors.email ? "outline-red-200" : "outline-green-200"
-              }`}
-              id="email"
-              type="email"
-              placeholder="이메일을 입력해주세요"
-              {...register("email", {
-                required: true,
-                pattern: {
-                  value: /^\S+@\S+\.\S+$/,
-                  message: "이메일 형식으로 작성해 주세요.",
-                },
-              })}
-            ></input>
-            {errors.email && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.email.message}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <label
-              htmlFor="password"
-              className="text-md font-normal text-gray-500"
-            >
-              비밀번호
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px] bg-gray-100 placeholder:text-md placeholder:text-gray4-00 ${
-                errors.password ? "outline-red-200" : "outline-green-200"
-              }`}
-              id="password"
-              type="password"
-              placeholder="비밀번호를 입력해주세요"
-              {...register("password", {
-                required: true,
-                minLength: {
-                  value: 8,
-                  message: "8자 이상 작성해 주세요",
-                },
-              })}
-            ></input>
-            {errors.password && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.password.message}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <label
-              htmlFor="passwordConfirmation"
-              className="text-md font-normal text-gray-500"
-            >
-              비밀번호 확인
-            </label>
-            <input
-              className={`rounded-[10px] px-5 py-[10.5px] mb-[10px] bg-gray-100 placeholder:text-md placeholder:text-gray4-00 ${
-                errors.passwordConfirmation
-                  ? "outline-red-200"
-                  : "outline-green-200"
-              }`}
-              id="passwordConfirmation"
-              type="password"
-              placeholder="비밀번호를 입력해주세요"
-              {...register("passwordConfirmation", {
-                required: true,
-                validate: (value) =>
-                  value === password || "비밀번호가 일치하지 않습니다",
-              })}
-            ></input>
-            {errors.passwordConfirmation && (
-              <span className="text-xs text-red-200" role="alert">
-                {errors.passwordConfirmation.message}
-              </span>
-            )}
-          </div>
+          <FormInput
+            id="name"
+            label="이름"
+            placeholder="이름을 입력해 주세요"
+            type="text"
+            register={register("name", {
+              required: true,
+              maxLength: {
+                value: 10,
+                message: "열 자 이하로 작성해 주세요",
+              },
+            })}
+            error={errors.name}
+          />
+          <FormInput
+            id="email"
+            label="이메일"
+            placeholder="이메일을 입력해주세요"
+            type="text"
+            register={register("email", {
+              required: true,
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "이메일 형식으로 작성해 주세요.",
+              },
+            })}
+            error={errors.email}
+          />
+          <FormInput
+            id="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
+            type="password"
+            register={register("password", {
+              required: true,
+              minLength: {
+                value: 8,
+                message: "8자 이상 작성해 주세요",
+              },
+            })}
+            error={errors.password}
+          />
+          <FormInput
+            id="passwordConfirmation"
+            label="비밀번호확인"
+            placeholder="비밀번호를 입력해주세요"
+            type="password"
+            register={register("passwordConfirmation", {
+              required: true,
+              validate: (value) =>
+                value === password || "비밀번호가 일치하지 않습니다",
+            })}
+            error={errors.password}
+          />
           <FilledButton type="submit" disabled={isSubmitting}>
             회원가입
           </FilledButton>
