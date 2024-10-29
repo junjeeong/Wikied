53d560c12a193c75a6aeab425fee557307f8ae0d
JSM <venise5224@gmail.com>
2024-10-30 01:09:30 +0900

    Fix: 상태관리에서 라우터로 수정

--- src/containers/test.tsx
+++ src/containers/test.tsx
@@ -14,75 +14,31 @@ const TotalArticlesContainer = ({
   totalArticles: initialTotalArticles,
   totalCount,
 }: TotalArticlesContainerProps) => {
-  const [articles, setArticles] = useState(initialTotalArticles);
-  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState(initialTotalArticles);
   const [isPC, setIsPC] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
   const totalPage = Math.ceil(totalCount / PAGE_SIZE);
 
-  const router = useRouter();
   const { q = "", order = "recent", page = 1 } = router.query;
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await getArticles({
+        page: Number(page),
+        pageSize: PAGE_SIZE,
+        orderBy: order as string,
+        keyword: q as string,
+      });
+      setData(response.list);
+    } catch (error) {
+      console.error("게시글을 받아오는 중 에러가 발생했습니다.", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
-
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   router.push({
-  //     pathname: router.pathname,
-  //     query: { q: e.target.value, order, page: 1 },
-  //   });
-  // };
-
-  const handleInputSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    router.push({
-      pathname: router.pathname,
-      query: { q: inputValue, order, page: 1 },
-    });
-  };
-
-  const handleButtonClick = () => {
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, page: 1 },
-    });
-  };
-
-  const handleOrderChange = (orderValue: string) => {
-    router.push({
-      pathname: router.pathname,
-      query: { q, order: orderValue, page: 1 },
-    });
-  };
-
-  const handlePageChange = (currentPage: number) => {
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, page: currentPage },
-    });
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await getArticles({
-          page: Number(page),
-          pageSize: PAGE_SIZE,
-          orderBy: order as string,
-          keyword: q as string,
-        });
-        setArticles(response.list);
-      } catch (error) {
-        console.error("게시글을 받아오는 중 에러가 발생했습니다.", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [page, order, q]);
 
   const handleResize = () => {
     if (window.innerWidth > 1200) {
@@ -92,11 +48,46 @@ const TotalArticlesContainer = ({
     }
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    router.push({
+      pathname: "/boards",
+      query: { q: e.target.value, order, page: 1 },
+    });
+  };
+
+  const handleInputSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push({ pathname: "/boards", query: { q, order, page: 1 } });
+  };
+
+  const handleButtonClick = () => {
+    router.push({ pathname: "/boards", query: { q, order, page: 1 } });
+  };
+
+  const handleOrderChange = (orderValue: string) => {
+    router.push({
+      pathname: "/boards",
+      query: { q, order: orderValue, page: 1 },
+    });
+  };
+
+  const handlePageChange = (page: number) => {
+    router.push({ pathname: "/boards", query: { q, order, page } });
+  };
+
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    if (Number(page) === 1 && order === "recent" && !q) {
+      setData(initialTotalArticles);
+    } else {
+      fetchData();
+    }
+  }, [page, order, q, initialTotalArticles]);
 
   const size = isPC ? "medium" : "small";
 
@@ -106,7 +97,7 @@ const TotalArticlesContainer = ({
         <div className="flex gap-[20px] items-center">
           <SearchInput
             size={size}
-            value={inputValue}
+            value={q as string}
             onChange={handleInputChange}
             onSubmit={handleInputSubmit}
           />
@@ -118,7 +109,7 @@ const TotalArticlesContainer = ({
           <OrderDropdown order={order as string} onChange={handleOrderChange} />
         </div>
       </div>
-      <TotalArticles totalArticles={articles} />
+      <TotalArticles totalArticles={data} />
       <div className="my-[60px] flex justify-center">
         <PaginationBar
           totalPage={totalPage}
