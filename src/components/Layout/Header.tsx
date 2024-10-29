161edb19e5334450f5dac31ce2a375dd39c0e2bb
junjeeong <wn8624@naver.com>
2024-10-29 12:12:12 +0900

    Reafactor: Header컴포넌트 상단고정, UI 컴포넌트 관심사 분리

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,22 +1,17 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { SearchInput } from "@/components/SearchInput";
-import Image from "next/image";
-import Link from "next/link";
-import useAuthStore from "@/store/AuthStore";
-import Alarm from "/public/icons/ic_alarm.svg";
-import LoginDropdown from "../ui/Dropdown/LoginDropdown";
-import LogoutDropdown from "../ui/Dropdown/LogoutDropdown";
-import FilledButton from "../ui/Button/FilledButton";
+import Logo from "../Logo";
+import Navigation from "../Navigation";
+import UserMenu from "../UserMenu";
 
 export const Header = () => {
   const router = useRouter();
   const [isMobile, setIsMobile] = useState(false);
   const [searchedName, setSearchedName] = useState("");
-  const { isLoggedIn } = useAuthStore();
 
   const handleResize = () => {
-    setIsMobile(innerWidth < 768);
+    setIsMobile(window.innerWidth < 768);
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -41,59 +36,20 @@ export const Header = () => {
   }, []);
 
   return (
-    <div className="w-screen p-6 h-20 bg-gray-50 flex justify-between items-center">
-      <Link href="/" aria-label="홈으로 이동">
-        <Image
-          src={"/logo/wikied.svg"}
-          width={107}
-          height={30}
-          alt="wikied logo"
+    <div className="w-screen p-6 h-20 bg-gray-50 flex justify-between items-center fixed z-[999]">
+      <Logo />
+      <div className="Mobile:hidden ml-auto">
+        <SearchInput
+          size="small"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          value={searchedName}
         />
-      </Link>
-
-      <ul className="flex items-center flex-row-reverse flex-grow gap-10 mr-10">
-        <li
-          className={`${
-            router.pathname === "/boards" ? "font-bold" : "text-gray-400"
-          } text-md hover:font-bold Mobile:hidden`}
-        >
-          <Link href="/boards" aria-label="자유 게시판">
-            자유 게시판
-          </Link>
-        </li>
-        <li
-          className={`${
-            router.pathname === "/wikilist" ? "font-bold" : "text-gray-400"
-          } text-md hover:font-bold Mobile:hidden`}
-        >
-          <Link href="/wikilist" aria-label="모든 위키">
-            모든 위키
-          </Link>
-        </li>
-        <li className="Mobile:hidden">
-          <SearchInput
-            size="small"
-            onChange={handleChange}
-            onSubmit={handleSubmit}
-            value={searchedName}
-          />
-        </li>
-      </ul>
-
-      {isLoggedIn ? (
-        <div className="flex items-center">
-          <Alarm className="cursor-pointer text-gray-400 hover:text-gray-500 Mobile:hidden" />
-          <div className="ml-[20px]">
-            <LoginDropdown />
-          </div>
-        </div>
-      ) : isMobile ? (
-        <LogoutDropdown />
-      ) : (
-        <Link href="/login">
-          <FilledButton>로그인</FilledButton>
-        </Link>
-      )}
+      </div>
+      <div className="mx-10">
+        <Navigation />
+      </div>
+      <UserMenu isMobile={isMobile} />
     </div>
   );
 };
