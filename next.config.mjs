8c81bbf380fb7d1ee10ae9c671772758fa3d11ed
jjeankim <miyukikj35@gmail.com>
2024-10-29 13:26:26 +0900

    Chore: dev merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"], ["cdn.pixabay.com"],
+    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com", "cdn.pixabay.com"],
   },
   webpack: (config) => {
     config.module.rules.push({
