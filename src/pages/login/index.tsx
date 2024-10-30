958855fdba77e6e6b5ef34c6c80da025452a5ab0
jjeankim <miyukikj35@gmail.com>
2024-10-30 16:15:31 +0900

    Rename: id => name

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -40,7 +40,7 @@ const Login = () => {
     if (isLoggedIn) {
       const user = useAuthStore.getState().user;
       if (user?.profile) {
-        router.push(`/wiki/${user.profile.id}`);
+        router.push(`/wiki/${user.profile.name}`);
       } else {
         router.push("/quiz-settings");
       }
