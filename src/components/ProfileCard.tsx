9960f0f02229da12af17488906317944530fe43b
DrowsyzzZ <daenamu222@naver.com>
2024-10-30 16:28:21 +0900

    Fix: 반응형 layout 수정

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -59,12 +59,12 @@ const ProfileCard = ({ userProfile, isMe, isEditing }: ProfileCardProps) => {
       className={`transition-all ease-in-out ${
         isMe && isEditing
           ? "w-[400px] h-[828px] p-10 gap-10 pb-9 Tablet:flex-col Tablet:h-[388px] Tablet:px-4 Tablet:pb-[37px] Tablet:gap-8 Mobile:flex-col Mobile:h-[511px] Mobile:pt-6 Mobile:px-[34px] Mobile:pb-[17px] Mobile:gap-6"
-          : `w-80 h-[671px] p-[30px] gap-[30px] Tablet:gap-10 Mobile:pt-[15px] Mobile:px-5 Mobile:pb-[41px] Mobile:gap-5 ${
+          : `PC:w-80 h-[671px] p-[30px] gap-[30px] Tablet:gap-10 Mobile:pt-[15px] Mobile:px-5 Mobile:pb-[41px] Mobile:gap-5 ${
               isExpanded
                 ? "Tablet:h-[270px] Mobile:h-64"
                 : "Tablet:h-[130px] Mobile:h-[126px]"
             }`
-      } flex relative text-md rounded-[10px] shadow-[0_4px_20px_#00000014] PC:flex-col Tablet:w-[624px] Tablet:pt-5 Mobile:text-xs Mobile:w-[335px] mx-auto`}
+      } flex relative text-md rounded-[10px] shadow-[0_4px_20px_#00000014] w-full PC:flex-col  Tablet:pt-5 Mobile:text-xs  mx-auto`}
     >
       <div
         className={`${
@@ -122,8 +122,10 @@ const ProfileCard = ({ userProfile, isMe, isEditing }: ProfileCardProps) => {
         className={`${
           isMe && isEditing
             ? "Tablet:h-[228px] Tablet:flex-wrap Mobile:flex-wrap"
-            : "Tablet:gap-1 Mobile:w-[213px] Mobile:gap-2"
-        } ${isExpanded ? "" : "overflow-hidden"} flex flex-col gap-4 `}
+            : "Tablet:gap-1 Mobile:gap-2"
+        } ${
+          isExpanded ? "" : "overflow-hidden"
+        } flex flex-col content-between gap-4 w-full`}
       >
         <ProfileInput
           isMe={isMe}
