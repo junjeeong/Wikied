c4506ef6f4dfc2cd7f455bcda4849e450bfa60bd
junjeeong <wn8624@naver.com>
2024-11-02 16:38:06 +0900

    Feat: CustomLink 눌렀을 때 url 복사 toast 기능 추가

--- src/containers/SearchedWikiCard.tsx
+++ src/containers/SearchedWikiCard.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { Profile } from "@/types/types";
 import ProfileSVG from "/public/icons/ic_profile.svg";
 import CustomLinkContainer from "./CustomLinkContainer";
+import Link from "next/link";
 
 interface SearchedWikiCardProps {
   info: Profile;
@@ -10,27 +11,22 @@ interface SearchedWikiCardProps {
 const SearchedWikiCard = ({ info }: SearchedWikiCardProps) => {
   const router = useRouter();
 
-  const handleClick = () => {
-    router.push(`/${info.name}`);
-  };
-
   return (
-    <div
-      onClick={handleClick}
-      className="flex relative w-[859px] h-[142px] px-[36px] py-[24px] border rounded-[10px] shadow-[0_4px_20px_#00000014]  cursor-pointer hover:scale-105 transition-all Tabel:w-[696px] Tablet:h-[142px] Mobile:w-[334px] Mobile:h-[150px] "
-    >
-      <div className="flex gap-[32px] flex-grow">
-        <ProfileSVG className="w-[85px] h-[85px] text-gray-300" />
-        <div>
-          <h2 className="text-gray-500 text-2xl">{info.name}</h2>
-          <span className="mt-[14px] text-gray-400">
-            {info.city}, {info.nationality} <br />
-            {info.job}
-          </span>
+    <div className="flex relative w-[859px] h-[142px] px-[36px] py-[24px] border rounded-[10px] shadow-[0_4px_20px_#00000014]  cursor-pointer hover:scale-105 transition-all Tabel:w-[696px] Tablet:h-[142px] Mobile:w-[334px] Mobile:h-[150px] ">
+      <Link href={`${info.name}`}>
+        <div className="flex gap-[32px] flex-grow">
+          <ProfileSVG className="w-[85px] h-[85px] text-gray-300" />
+          <div>
+            <h2 className="text-gray-500 text-2xl">{info.name}</h2>
+            <span className="mt-[14px] text-gray-400">
+              {info.city}, {info.nationality} <br />
+              {info.job}
+            </span>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="absolute bottom-[24px] right-[36px]">
-        {/* <CustomLinkContainer /> */}
+        <CustomLinkContainer link={`/wiki/${info.name}`} />
       </div>
     </div>
   );
