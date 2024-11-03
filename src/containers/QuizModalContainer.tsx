f9fd3ac8d65fd861102f37f82ca12990f3c89c87
jjeankim <miyukikj35@gmail.com>
2024-11-03 14:23:31 +0900

    Fix: wiki page로 입력값 전달 prop 생성

--- src/containers/QuizModalContainer.tsx
+++ src/containers/QuizModalContainer.tsx
@@ -8,24 +8,21 @@ interface QuizModalContainerProps {
   isOpen: boolean;
   onClose: () => void;
   code: string;
-  onSubmitSuccess?:(registeredAt:string) => void;
-  isMe:boolean;
+  onSubmitSueccess?: (quizAnswer: string) => void;
 }
 
 const QuizModalContainer = ({
   isOpen,
   onClose,
   code,
-  onSubmitSuccess, //시간을 돌려줌
-  isMe,
+  onSubmitSueccess,
 }: QuizModalContainerProps) => {
   const [question, setQuestion] = useState(""); //사용자 질문
   const [quizAnswer, setQuizAnswer] = useState(""); //입력값을 관리
   const [errorMessage, setErrorMessage] = useState(""); //에러값 상태확인
 
-
   useEffect(() => {
-    if (code && !isMe) {
+    if (code) {
       const getQuestion = async () => {
         const res = await getUserProfile(code);
         setQuestion(res?.data.securityQuestion);
@@ -42,28 +39,27 @@ const QuizModalContainer = ({
   const handleSubmit = async () => {
     try {
       if (code) {
-        const res = await postProfilePing({ securityAnswer: quizAnswer }, code);
+        await postProfilePing({ securityAnswer: quizAnswer }, code);
 
-        if (onSubmitSuccess) {
-          onSubmitSuccess(res.data.registeredAt)
+        if (onSubmitSueccess) {
+          onSubmitSueccess(quizAnswer);
         }
-          // API 호출이 성공적으로 완료되면 모달을 닫음
-          setQuizAnswer("")
-          setErrorMessage("");
-          onClose();
+        setErrorMessage("");
+        onClose();
       }
     } catch (error) {
       if (error instanceof AxiosError) {
-        setQuizAnswer("");
         setErrorMessage("정답이 아닙니다. 다시 시도해 주세요.");
       } else {
         console.error(error);
       }
+    } finally {
+      setQuizAnswer("");
     }
   };
 
-  if (isMe || !isOpen) {
-    return null
+  if (!isOpen) {
+    return null;
   }
 
   return (
