82225cd5fb13754ee8e8e8f6ec810758e17b04fa
jjeankim <159109607+jjeankim@users.noreply.github.com>
2024-11-04 11:55:49 +0900

    Delete src/lib/sanitizeHtml.ts

--- src/lib/sanitizeHtml.ts
+++
@@ -1,8 +0,0 @@
-import DOMPurify from "dompurify";
-import { JSDOM } from "jsdom";
-
-const sanitizeHtml = (html: string) => {
-  return DOMPurify(new JSDOM("<!DOCTYPE html>").window).sanitize(html);
-};
-
-export default sanitizeHtml;
