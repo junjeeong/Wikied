69388df7314463743f5234f553b9e27f45e827b6
junjeeong <wn8624@naver.com>
2024-11-05 01:29:14 +0900

    Fix :/Search 페이지 반응형

--- src/containers/SearchedWikiCard.tsx
+++ src/containers/SearchedWikiCard.tsx
@@ -1,32 +1,37 @@
-import { useRouter } from "next/router";
 import { Profile } from "@/types/types";
 import ProfileSVG from "/public/icons/ic_profile.svg";
 import CustomLinkContainer from "./CustomLinkContainer";
 import Link from "next/link";
+import useViewport from "@/hooks/useViewport";
 
 interface SearchedWikiCardProps {
   info: Profile;
 }
 
 const SearchedWikiCard = ({ info }: SearchedWikiCardProps) => {
-  const router = useRouter();
+  const { isMobile } = useViewport();
 
   return (
-    <div className="flex relative w-[859px] h-[142px] px-[36px] py-[24px] border rounded-[10px] shadow-[0_4px_20px_#00000014]  cursor-pointer hover:scale-105 transition-all Tabel:w-[696px] Tablet:h-[142px] Mobile:w-[334px] Mobile:h-[150px] ">
+    <div className=" relative flex w-[859px] h-[142px] px-[36px] py-[24px] border rounded-[10px] shadow-[0_4px_20px_#00000014]  cursor-pointer hover:scale-105 transition-all Tabel:w-[696px] Tablet:h-[142px] Mobile:w-[334px] Mobile:h-[150px] ">
       <Link href={`/wiki/${info.name}`}>
-        <div className="flex gap-[32px] flex-grow">
-          <ProfileSVG className="w-[85px] h-[85px] text-gray-300" />
-          <div>
-            <h2 className="text-gray-500 text-2xl">{info.name}</h2>
-            <span className="mt-[14px] text-gray-400">
+        <div className="flex gap-[32px]">
+          <ProfileSVG className="w-[85px] h-[85px] text-gray-300 Mobile:w-[60px] Mobile:h-[60px]" />
+          <div className="w-[350px]">
+            <h2 className="text-gray-500 text-2xl Mobile:text-lg">
+              {info.name}
+            </h2>
+            <span className="mt-[14px] text-gray-400 Mobile: text-sm">
               {info.city}, {info.nationality} <br />
               {info.job}
             </span>
           </div>
         </div>
       </Link>
-      <div className="absolute bottom-[24px] right-[36px]">
-        <CustomLinkContainer link={`/wiki/${info.name}`} />
+      <div className="absolute bottom-[24px] right-[36px] Mobile:bottom-[15px] Mobile:right-[21px]">
+        <CustomLinkContainer
+          link={`/wiki/${info.name}`}
+          size={isMobile ? "small" : "medium"}
+        />
       </div>
     </div>
   );
