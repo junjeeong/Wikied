78743b068b89a9d9e73ad8a28521bf001b59b2dc
junjeeong <wn8624@naver.com>
2025-02-26 16:32:50 +0900

    api 함수 -> comment.ts  에러 핸들링 개선

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -21,7 +21,7 @@ export const postSignUp = async (body: PostSignUpQuery) => {
     const res = await instance.post(`/auth/signUp`, body);
     return handleSuccess(res, "회원가입에 성공했습니다.");
   } catch (err) {
-    handleError(err as AxiosError);
+    return handleError(err as AxiosError);
   }
 }; //예외처리를 singUp 페이지에서 하고, 에러 메세지를 처리함
 
